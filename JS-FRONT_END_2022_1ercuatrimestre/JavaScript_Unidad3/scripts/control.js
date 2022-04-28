c56bd31243afd3f9c7f59dce6a03c2229ffd96a3
MatiasDSR <matiasdsanchezr@gmail.com>
2022-04-28 08:01:47 -0300

    Actualizacion de la actividad obligatoria de Bootstrap Unidad 1

---
+++ JS-FRONT_END_2022_1ercuatrimestre/JavaScript_Unidad3/scripts/control.js
@@ -0,0 +1,13 @@
+const button = document.getElementById("submit-button");
+const user = document.getElementById("user");
+const password = document.getElementById("password");
+const placeholder = document.getElementById("placeholder");
+
+function test() {
+	tag = document.createElement("p");
+	tag.innerHTML;
+	user.innerText = "asdasdas";
+	placeholder.appendChild(tag);
+}
+
+button.addEventListener("click", test, false);
